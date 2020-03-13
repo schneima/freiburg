aa445afc4d750209b024239afdcbf4a3a82ab8ce
Markus <ms2301@gmx.de>
2020-03-13 21:08:38 +0100

    Event Update
    - add new event info

--- content/home.php
+++ content/home.php
@@ -2,7 +2,7 @@
 
 <?php
 
-$refDate=new DateTime("2020-01-05 23:50:00");
+$refDate=new DateTime("2020-05-23 23:50:00");
 $nowDate=new DateTime('NOW');
 
 if($nowDate < $refDate)
@@ -23,16 +23,10 @@ if($nowDate < $refDate)
            <img
                width="500px"
                class="shaddow center"
-               alt="Theater 2020"
-               src="./images/news/70.jpg" />
+               alt="WeinProbenKonzert 2020"
+               src="./images/konzerte/2020_05_Plakat.jpg" />
         </p> 
     </a>
-    <p class="center">
-        <br>
-        Zur öffentlichen Weihnachtsfeier am<br>
-    <span class="bold red">Sonntag, den 05. Januar 2020 um 19.30 Uhr</span>
-        <br>laden wir Sie recht herzlich ein. 
-        <br><a href="?content=aktuell">Weitere Informationen</a></p>
 </div>
 
 <div style="<?php echo $staticMessageStyle;  ?>">
