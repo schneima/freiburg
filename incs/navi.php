c35845036211f0abc74b8cf166c81a8687abe889
Markus <ms2301@gmx.de>
2020-07-25 12:25:17 +0200

    Cleanup
    - minor whitespace cleanup

--- incs/navi.php
+++ incs/navi.php
@@ -5,27 +5,27 @@
     <ul>
       <li><a href="?content=aktuell"> Aktuelles </a></li>
       <li><a href="?content=termine"> Termine </a></li>
-        <li>
-            <span> Über uns </span>
-            <ul>
-              <li><a href="?content=about"> Kapelle </a></li>
-              <li><a href="?content=dirigent"> Dirigent </a></li>
-              <li><a href="?content=jugend"> Jugendausbildung </a>
-              <li><a href="?content=presse"> Presseberichte </a></li>
-              <li><a href="?content=mitteilungen"> Vereinsmitteilungen </a></li>
-              <li><a href="?content=chronik"> Chronik </a></li>
-            </ul>
-        </li>
-        <li><a href="?content=beitritt"> Mitglied werden</a></li>
-        <li><a href="?content=kontakt"> Kontakt </a></li>
-      
-        <?php
-        if(isset($logoutBtn))
-        {
-          echo "<a>";
-            echo $logoutBtn;
-            echo "</a>";
-        }
-    ?>      
-      </ul>
+      <li>
+        <span> Über uns </span>
+        <ul>
+          <li><a href="?content=about"> Kapelle </a></li>
+          <li><a href="?content=dirigent"> Dirigent </a></li>
+          <li><a href="?content=jugend"> Jugendausbildung </a>
+          <li><a href="?content=presse"> Presseberichte </a></li>
+          <li><a href="?content=mitteilungen"> Vereinsmitteilungen </a></li>
+          <li><a href="?content=chronik"> Chronik </a></li>
+        </ul>
+      </li>
+      <li><a href="?content=beitritt"> Mitglied werden</a></li>
+      <li><a href="?content=kontakt"> Kontakt </a></li>
+
+      <?php
+      if(isset($logoutBtn))
+      {
+        echo "<a>";
+          echo $logoutBtn;
+          echo "</a>";
+      }
+      ?>
+    </ul>
 </nav>
