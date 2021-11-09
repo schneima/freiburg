73c576a357b9c217a9e4f12fdefa912872da7efd
Markus <ms2301@gmx.de>
2021-11-09 22:14:00 +0100

    Nov 2021 Event
    - adjust styles
    - add event info

--- content/home.php
+++ content/home.php
@@ -2,12 +2,11 @@
 
 <?php
 
-$refDate=new DateTime("2020-12-23 23:50:00");
+$refDate=new DateTime("2021-11-20 23:50:00");
 $nowDate=new DateTime('NOW');
 
 if($nowDate < $refDate)
     {
-    echo '';
         $staticMessageStyle="display: none; visibility: hidden; height: 0px;";
         $tempNewsStyle="visibility: visible;";
     }else{
@@ -16,52 +15,24 @@ if($nowDate < $refDate)
     }
 ?>
 
-<h1> Fronleichnam 2021</h1>
-
-<h3>
-Besondere Situationen erfordern besondere Maßnahmen
-</h3>
-<h3>
-
-Aus diesem Grund haben wir uns dieses Jahr etwas einfallen lassen zu unserem traditionellen Fronleichnamshock-Termin:
-</h3>
-
-<div>
-    <a href="images/news/fronleichnam-2021/flyer-1.jpeg"
-       rel="shadowbox['news75']">
-        <img name="images/news/fronleichnam-2021/flyer-1.jpeg" 
-             class="thumb" 
-             src="images/news/fronleichnam-2021/flyer-1.jpeg"
-             alt="Fronleichnamshock To Go 2021">
+<div style="<?php echo $tempNewsStyle;  ?>">
+    <a href="?content=aktuell">
+        <img
+            id="homeImage"
+            src="images/news/76_Herbstfeuer_S1.jpg"
+            lowsrc="images/news/76_Herbstfeuer_S1-min.jpg"
+            alt="2021 Herbstfeuer" />
     </a>
-
-    <a href="images/news/fronleichnam-2021/flyer-1.jpeg"
-       rel="shadowbox['news75']">
-    </a>
-
-</a>
-<p>
-
-<a href="media/2021-Fronleichnam-to-go-flyer.pdf" >
-Flyer zum Download
-</a>
-</p>
 </div>
 
 
 <!-- TODO: https://www.youtube.com/watch?v=flauMZdycgw
 -->
-<!--
 
 <div style="<?php echo $staticMessageStyle;  ?>">
         <h1>Herzlich Willkommen</h1>
-
     <img 
      id="homeImage" 
-     width="100%"
-     height="auto"
      src="images/zermatt_2019_title.jpg" 
      lowsrc="images/zermatt_2019_title_small.jpg">
 </div>
-
--->
